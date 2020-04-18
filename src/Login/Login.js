ec96eb77f1a3525588c004ee5bd0a9315b2ad00e
Ebrahim Salehi <eeeee.bbbbb.iiiii@gmail.com>
2020-04-18 17:13:59 +0430

    changes

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -7,19 +7,42 @@ import OtherParts from '../OtherParts';
 export default function Login() {
 
   const [checkAuth,setCheckAuth] = useState(false);
- 
-  function checkUserAuthentication(user,pass){
-    const isAuth = (user==='a@a.com' && pass==='a');
-    console.log('isAuth',isAuth);    
+  const [user,setUser] = useState('');
+  const [pass,setPass] = useState('');  
+
+  function handlerChangeUser(e){
+    const val = e.target.value;
+    console.log('handlerChangeUser',val);
+    
+    setUser(val);
+  }
+
+  function handlerChangePass(e){
+    const val = e.target.value;
+
+    console.log('handlerChangePass',val);
+
+    setPass(val);
+  }
+
+  const userInfo = {
+    user: 'a@a.com',
+    pass:'a'
+  };
+
+  function checkUserAuthentication(){
+    const isAuth = (user===userInfo.user && pass===userInfo.pass);
+    console.log('isAuth',isAuth,userInfo,user,pass);    
 
     setCheckAuth(isAuth);
   }
 
   return (
     <Form>
+     { !checkAuth && <>
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" defaultValue={user} onChange={handlerChangeUser}/>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -27,19 +50,23 @@ export default function Login() {
 
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" defaultValue={pass} onChange={handlerChangePass} />
       </Form.Group>
       <Form.Group controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
-      <Button variant="primary" type="submit" onClick={()=>{
+      <Button variant="primary"  onClick={()=>{
         checkUserAuthentication();
       }}>
         Submit
       </Button>
+      </>
+      }
       {
         checkAuth && <OtherParts />
       }
+
     </Form>
   );
+  
 }
